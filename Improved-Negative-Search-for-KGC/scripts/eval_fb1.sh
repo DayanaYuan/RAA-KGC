d3a7c47ee2212fbe68f0cbd80afa91204882f670
DayanaYuan <55037436+DayanaYuan@users.noreply.github.com>
2025-02-19 15:23:21 +0800

    Update eval_fb1.sh

--- Improved-Negative-Search-for-KGC/scripts/eval_fb1.sh
+++ Improved-Negative-Search-for-KGC/scripts/eval_fb1.sh
@@ -37,11 +37,11 @@ if [ "${task}" = "wiki5m_ind" ]; then
   neighbor_weight=0.0
 fi
 
-python3 -u /home/horanchen/ydy/study/code/5Improved-Negative-Search-for-KGC/evaluate.py \
+python3 -u evaluate.py \
 --task "FB15k237" \
 --is-test \
---eval-model-path "/home/horanchen/ydy/study/code/5Improved-Negative-Search-for-KGC/data/fb237_v1/checkpoint/fb237_tail_entity_2hop_neighbours_hard_negative_1pos1neg/model_best.mdl" \
+--eval-model-path "fb237_v1/checkpoint/fb237_tail_entity_2hop_neighbours_hard_negative_1pos1neg/model_best.mdl" \
 --neighbor-weight "${neighbor_weight}" \
 --rerank-n-hop "${rerank_n_hop}" \
---train-path "/home/horanchen/ydy/study/code/5Improved-Negative-Search-for-KGC/data/fb237_v1/train.txt.json" \
---valid-path "/home/horanchen/ydy/study/code/5Improved-Negative-Search-for-KGC/data/fb237_v1/test.txt.json" "$@"
+--train-path "fb237_v1/train.txt.json" \
+--valid-path "fb237_v1/test.txt.json" "$@"
