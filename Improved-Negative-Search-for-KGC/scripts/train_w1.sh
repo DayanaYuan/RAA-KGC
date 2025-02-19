798201f0ae0d7250da773b3cceb1d4e5e87f3db8
DayanaYuan <55037436+DayanaYuan@users.noreply.github.com>
2025-02-19 15:14:15 +0800

    Update train_w1.sh

--- Improved-Negative-Search-for-KGC/scripts/train_w1.sh
+++ Improved-Negative-Search-for-KGC/scripts/train_w1.sh
@@ -15,14 +15,14 @@ if [ -z "$DATA_DIR" ]; then
   DATA_DIR="${DIR}/data/${TASK}"
 fi
 
-python3 -u /home/horanchen/ydy/study/code/5Improved-Negative-neibor/main.py \
---model-dir "/home/horanchen/ydy/study/code/5Improved-Negative-neibor/WN18RR_v1/checkpoint/" \
---pretrained-model /home/horanchen/ydy/study/code/5Improved-Negative-neibor/bert-base-uncased \
+python3 -u main.py \
+--model-dir "/WN18RR_v1/checkpoint/" \
+--pretrained-model bert-base-uncased \
 --pooling mean \
 --lr 5e-5 \
 --use-link-graph \
---train-path "/home/horanchen/ydy/study/code/5Improved-Negative-neibor/WN18RR_v1/train.txt.json" \
---valid-path "/home/horanchen/ydy/study/code/5Improved-Negative-neibor/WN18RR_v1/valid.txt.json" \
+--train-path "WN18RR_v1/train.txt.json" \
+--valid-path "WN18RR_v1/valid.txt.json" \
 --task ${TASK} \
 --batch-size 32 \
 --print-freq 20 \
